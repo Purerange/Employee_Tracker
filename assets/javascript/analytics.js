e7c80c5dd1ecb3634854bddaf7964a4752c7b9e5
rgao <xinghoshi@gmail.com>
2018-10-04 21:06:17 -0700

    graph framework

---
+++ assets/javascript/analytics.js
@@ -0,0 +1,50 @@
+var config = {
+    apiKey: "AIzaSyCk-rZaCachMxJecTttRfbZMPHC1Z0EWQA",
+    authDomain: "employeetracker-3a59d.firebaseapp.com",
+    databaseURL: "https://employeetracker-3a59d.firebaseio.com",
+    projectId: "employeetracker-3a59d",
+    storageBucket: "",
+    messagingSenderId: "809093610593"
+  };
+
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
+var ctx = document.getElementById("myChart").getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 13, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+        }
+    }
+});
